f0561b1dca1e3387699eee4f14c34e623a6031b1
David Eilers <davidwesseleilers@hotmail.com>
2016-11-15 23:02:11 +0100

    Added functionality to roll in or roll out the blind

--- Central Unit/CherryPyTest.py
+++ Central Unit/CherryPyTest.py
@@ -10,111 +10,132 @@ class Default(object):
     def __init__(self):
         # establish connection with Arduino
         self.controller = UnoNetworkController()
-        self.test = 0
         self.controller.connect()
 
     # cherrypy.expose exposes a def as a webpage
     @cherrypy.expose
-    def index(self, percentage=0):
+    def index(self, control=""):
+
+        if control == "rollIn":
+            print("Rollin In...")
+            self.controller.rollOut(0)
+        if control == "rollOut":
+            print("Rollin Out...")
+            self.controller.rollOut(1)
 
         # convert int to str for display reasons
         lightValue = str(self.controller.getLight())
         tempValue = str(self.controller.getTemp())
         rolledOutValue = str(self.controller.getRolledOut())
 
+        # debug values
+        # lightValue = str(randint(10,30))
+        # tempValue = str(randint(10,30))
+        # rolledOutValue = str(1)
 
         return '''
         <html>
+            <head>
+                <title>Zeng Home | Project 2.1</title>
 
-  <head>
-    <title>Zeng Home | Project 2.1</title>
+                <!-- JS Scripts -->
+                <script type="text/javascript" src="/static/js/jquery-3.1.1.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.17.0/vis.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    <!-- JS Scripts -->
-    <script type="text/javascript" src="/static/js/jquery-3.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.17.0/vis.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+                <!-- Stylesheets -->
+                <link rel="stylesheet" href="/static/css/vis.css" type="text/css" />
+                <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+            </head>
 
-    <!-- Stylesheets -->
-    <link rel="stylesheet" href="/static/css/vis.css" type="text/css" />
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  </head>
+            <body>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <a href="settings">Settings Are Here</a>
+                            <form method="post">
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="control" value="rollIn" checked>Inrollen
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" name="control" value="rollOut">Uitrollen
+                                    </label>
+                                </div>
+                                <button type="submit" class="btn btn-default">Uitvoeren</button>
+                            </form>
+                            <br/>
+                            <br/>
+                            <div id="values">
+                                <label>Current Temperature: </label>
+                                <div id="currenttemp">''' + tempValue + ''' Cº</div><br>
+                                <label>Current Light: </label>
+                                <div id="currentlight">''' + lightValue + ''' Lx</div><br>
+                                <label>Status: </label>
+                                <div id="currentstatus">''' + rolledOutValue + '''</div><br>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+                            only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+                            software like Aldus PageMaker including versions of Lorem Ipsum
+                        </div>
+                        </div>
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div id="tempGraph"></div>
+                            </div>
+                            <div class="col-md-5 col-md-offset-1">
+                                <div id="lightGraph"></div>
+                        </div>
+                    </div>
+                </div>
 
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <a href="settings">Settings Are Here</a>
-          <br/>
-          <br/>
-          <div id="values">
-            <label>Current Temperature: </label>
-            <div id="currenttemp">''' + tempValue + ''' Cº</div><br>
-            <label>Current Light: </label>
-            <div id="currentlight">''' + lightValue + ''' Lx</div><br>
-            <label>Status: </label>
-            <div id="currentstatus">''' + rolledOutValue + '''</div><br>
-          </div>
-        </div>
-        <div class="col-md-8">
-          is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not
-          only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-          software like Aldus PageMaker including versions of Lorem Ipsum
-        </div>
-      </div>
-      <div class="row">&nbsp;</div>
-      <div class="row">
-        <div class="col-md-5">
-          <div id="tempGraph"></div>
-        </div>
-        <div class="col-md-5 col-md-offset-1">
-          <div id="lightGraph"></div>
-        </div>
-      </div>
-    </div>
-    <script type="text/javascript">
-    function ajax1() {
-        return $.ajax({
-            url : 'values',
-            type : 'POST',
-            dataType : 'json'
-          })
-    }
+                <script type="text/javascript">
+                function ajax1() {
+                    return $.ajax({
+                        url : 'values',
+                        type : 'POST',
+                        dataType : 'json'
+                      })
+                }
 
-      $(document).ready(function() {
-        //$(document).delay(1000);
+                  $(document).ready(function() {
 
-        var tempContainer = document.getElementById('tempGraph');
-        var lightContainer = document.getElementById('lightGraph');
-        var getTime = new Date();
-        var getTimePlus = new Date(getTime);
-        getTimePlus.setMinutes(getTime.getMinutes() + 5 )
-        var items = [
-          {x: getTime.getTime(), y: 0}
-        ];
-        var graph2d1data = new vis.DataSet(items);
-        var graph2d2data = new vis.DataSet(items);
-        var options = {
-          start: getTime.getTime(),
-          end: getTimePlus.getTime(),
-          autoResize: true
-        };
-        var graph2d1 = new vis.Graph2d(tempContainer, graph2d1data, options);
-        var graph2d2 = new vis.Graph2d(lightContainer, graph2d2data, options);
-        function updateGraph() {
-          $.when(ajax1()).done(function(result) {
-            console.log("Request Completed");
-            var getTime = new Date();
-            graph2d1data.update({x: getTime.getTime(), y: result['tempValue']});
-                graph2d2data.update({x: getTime.getTime(), y: result['lightValue']});
-                $("#currenttemp").html(result['tempValue'] + "Cº");
-                $("#currentlight").html(result['lightValue'] + "Lx");
-                $("#currentstatus").html(result['rolledOutValue']);
-            setTimeout(updateGraph(), 3000);
-          })
-        }
-      });
-    </script>
-
+                    var tempContainer = document.getElementById('tempGraph');
+                    var lightContainer = document.getElementById('lightGraph');
+                    var getTime = new Date();
+                    var getTimePlus = new Date(getTime);
+                    getTimePlus.setMinutes(getTime.getMinutes() + 5 )
+                    var items = [
+                      {x: getTime.getTime(), y: 0}
+                    ];
+                    var graph2d1data = new vis.DataSet(items);
+                    var graph2d2data = new vis.DataSet(items);
+                    var options = {
+                      start: getTime.getTime(),
+                      end: getTimePlus.getTime(),
+                      autoResize: true
+                    };
+                    var graph2d1 = new vis.Graph2d(tempContainer, graph2d1data, options);
+                    var graph2d2 = new vis.Graph2d(lightContainer, graph2d2data, options);
+                    function updateGraph() {
+                      $.when(ajax1()).done(function(result) {
+                        console.log("Request Completed");
+                        var getTime = new Date();
+                        graph2d1data.update({x: getTime.getTime(), y: result['tempValue']});
+                            graph2d2data.update({x: getTime.getTime(), y: result['lightValue']});
+                            $("#currenttemp").html(result['tempValue'] + "Cº");
+                            $("#currentlight").html(result['lightValue'] + "Lx");
+                            $("#currentstatus").html(result['rolledOutValue']);
+                        setTimeout(updateGraph(), 3000);
+                      })
+                    }
+                  });
+                </script>
             </body>
         </html>'''
 
